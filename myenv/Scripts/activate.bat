d16ebba2a3bbb3d8839c9a889b5b588ce3d50f7c
radhikab1 <radhika3663@gmail.com>
2023-11-05 10:32:20 -0800

    some update

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rambo\Documents\rad\UBC BSC\Personal Projects\Django\myenv
+set VIRTUAL_ENV=C:\Users\rambo\Documents\rad\UBC BSC\Personal Projects\GradeSaver-backend\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
